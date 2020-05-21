b1c8b0f45bf1e93f85325828f62ede12cd8415b6
Gregg Kellogg <gregg@greggkellogg.net>
2020-05-20 17:19:02 -0700

    Update Test class definition to fix yardoc issue.

--- lib/shex/extensions/test.rb
+++ lib/shex/extensions/test.rb
@@ -6,21 +6,23 @@
 # @see http://shex.io/extensions/Test/
 require 'shex'
 
-class ShEx::Test < ShEx::Extension("http://shex.io/extensions/Test/")
-  # (see ShEx::Extension#visit)
-  def visit(code: nil, matched: nil, depth: 0, **options)
-    str = if md = /^ *(fail|print) *\( *(?:(\"(?:[^\\"]|\\")*\")|([spo])) *\) *$/.match(code.to_s)
-      md[2] || case md[3]
-      when 's' then matched.subject
-      when 'p' then matched.predicate
-      when 'o' then matched.object
-      else          matched.to_sxp
-      end.to_s
-    else
-      matched ? matched.to_sxp : 'no statement'
-    end
+module ShEx
+  Test = Class.new(ShEx::Extension("http://shex.io/extensions/Test/")) do
+    # (see ShEx::Extension#visit)
+    def visit(code: nil, matched: nil, depth: 0, **options)
+      str = if md = /^ *(fail|print) *\( *(?:(\"(?:[^\\"]|\\")*\")|([spo])) *\) *$/.match(code.to_s)
+        md[2] || case md[3]
+        when 's' then matched.subject
+        when 'p' then matched.predicate
+        when 'o' then matched.object
+        else          matched.to_sxp
+        end.to_s
+      else
+        matched ? matched.to_sxp : 'no statement'
+      end
 
-    $stdout.puts str
-    return !md || md[1] == 'print'
+      $stdout.puts str
+      return !md || md[1] == 'print'
+    end
   end
 end
